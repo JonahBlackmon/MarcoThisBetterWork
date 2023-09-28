d7daf9fe9d1e92915b6af39a7f914de700e64e71
JonahBlackmon <142601783+JonahBlackmon@users.noreply.github.com>
2023-09-28 17:28:59 +0000

    Added support for command line

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 import GCD.*;
 public class Main {
-    public static void main(String args[]) {
-       GCD.gcd Test = new GCD.gcd();
-       System.out.println(Test.GCD(10, 10));
-
+    public static void main(String[] args) {
+    int a = 0;
+    int b = 0;
+    if (args.length > 1) {
+       a = Integer.parseInt(args[0]);
+       b = Integer.parseInt(args[1]);
+    } else System.out.println("Error, Invalid Inputs");    
+      GCD.gcd test = new GCD.gcd();
+      System.out.println(test.GCD(a, b));
     }
 }
---
+++ runjava.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java @/tmp/cp_ax2nayino6ctezrr9w66ully7.argfile Main "$@"
